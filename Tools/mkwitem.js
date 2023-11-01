6ef8420ae264de31ab21b75ec44642494041bf08
Wexos <wexosmk@gmail.com>
2023-11-01 02:37:40 +0100

    Ability to remove items. Color-code based on percentage.

--- Tools/mkwitem.js
+++ Tools/mkwitem.js
@@ -1,4 +1,23 @@
 function initTable() {
+    const tableElem = document.getElementById("tableItem");
+
+    // Create table
+    for (const item of ITEM_INFO) {
+        const columnElem = document.createElement("th");
+        const imgElem = document.createElement("img");
+
+        columnElem.onclick = (e) => toggleItem(e.target);
+
+        imgElem.src = `../Images/Items/${item[0]}`;
+        imgElem.style.maxWidth = "64px";
+        imgElem.style.width = "100%";
+        imgElem.style.height = "100%";
+        imgElem.title = item[1];
+
+        columnElem.appendChild(imgElem);
+        tableElem.rows[0].appendChild(columnElem);
+    }
+
     // Player count options
     const playerSelectElem = document.getElementById("selectPlayerCount");
 
@@ -18,15 +37,41 @@ function initTable() {
 function updateTable() {
     const tableElem = document.getElementById("tableItem");
 
-    // Delete old entries
-    while (tableElem.rows.length > 1) {
-        tableElem.deleteRow(-1);
-    }
-
     const playerSelectElem = document.getElementById("selectPlayerCount");
     const playerCount = playerSelectElem.value;
 
     let items = structuredClone(ITEM_TABLE[playerCount]);
+    let disabledItems = new Set();
+
+    // Check what items are disabled
+    for (let i = 0; i < ITEM_COUNT; i++) {
+        const columnElem = tableElem.rows[0].cells[i + 1];
+
+        if (columnElem.children[0].classList.contains("ITEM_DISABLED")) {
+            for (let j = 0; j < items.length; j++) {
+                items[j][i] = 0;
+            }
+
+            disabledItems.add(i);
+        }
+    }
+
+    // Normalize probabilities
+    for (let i = 0; i < items.length; i++) {
+        let sum = 0;
+
+        for (let j = 0; j < items[i].length; j++) {
+            sum += items[i][j] / 100;
+        }
+
+        if (sum > 0) {
+            for (let j = 0; j < items[i].length; j++) {
+                items[i][j] /= sum;
+            }
+        }
+    }
+
+    // Find max probabilities
     let maxProbs = [];
 
     for (let i = 0; i < ITEM_COUNT; i++) {
@@ -43,6 +88,12 @@ function updateTable() {
         maxProbs.push(maxProb);
     }
 
+    // Delete old rows
+    while (tableElem.rows.length > 1) {
+        tableElem.deleteRow(-1);
+    }
+
+    // Create table
     for (let i = 0; i < items.length; i++) {
         const rowElem = document.createElement("tr");
         tableElem.appendChild(rowElem);
@@ -54,29 +105,45 @@ function updateTable() {
         posElem.style.textAlign = "right";
 
         for (let j = 0; j < items[i].length; j++) {
-            const itemPerc = items[i][j];
-
             const itemElem = document.createElement("td");
             rowElem.appendChild(itemElem);
 
-            itemElem.innerHTML = `${itemPerc}%`;
-            itemElem.style.textAlign = "right";
+            if (disabledItems.has(j)) {
+                itemElem.style.backgroundColor = "black";
+            }
+            else {
+                const itemPerc = Math.round(items[i][j] * 10) / 10;
 
-            if (itemPerc > 0) {
-                const COLOR_LOW = [255, 255, 255];
-                const COLOR_HIGH = [200, 0, 0];
+                itemElem.innerHTML = `${itemPerc}%`;
+                itemElem.style.textAlign = "right";
 
-                const interpolation = itemPerc / maxProbs[j];
-                const color = [
-                    Math.round(COLOR_LOW[0] * (1 - interpolation) + COLOR_HIGH[0] * interpolation),
-                    Math.round(COLOR_LOW[1] * (1 - interpolation) + COLOR_HIGH[1] * interpolation),
-                    Math.round(COLOR_LOW[2] * (1 - interpolation) + COLOR_HIGH[2] * interpolation)
-                    ];
-                const colorStr =  `#${color[0].toString(16).padStart(2, "0")}${color[1].toString(16).padStart(2, "0")}${color[2].toString(16).padStart(2, "0")}`;
-                itemElem.style.backgroundColor = colorStr;
+                if (itemPerc > 0) {
+                    const COLOR_LOW = [255, 255, 255];
+                    const COLOR_HIGH = [200, 0, 0];
+
+                    const interpolation = itemPerc / maxProbs[j];
+                    const color = [
+                        Math.round(COLOR_LOW[0] * (1 - interpolation) + COLOR_HIGH[0] * interpolation),
+                        Math.round(COLOR_LOW[1] * (1 - interpolation) + COLOR_HIGH[1] * interpolation),
+                        Math.round(COLOR_LOW[2] * (1 - interpolation) + COLOR_HIGH[2] * interpolation)
+                        ];
+                    const colorStr = `#${color[0].toString(16).padStart(2, "0")}${color[1].toString(16).padStart(2, "0")}${color[2].toString(16).padStart(2, "0")}`;
+                    itemElem.style.backgroundColor = colorStr;
+                }
             }
         }
     } 
+}
+
+function toggleItem(elem) {
+    if (elem.classList.contains("ITEM_DISABLED")) {
+        elem.classList.remove("ITEM_DISABLED");
+    }
+    else {
+        elem.classList.add("ITEM_DISABLED");
+    }
+
+    updateTable();
 }
 
 const ITEM_COUNT = 19;
@@ -182,3 +249,25 @@ const ITEM_TABLE = {
         [   0,   0,    0,   0,    0,    5,   0,   0,  20, 17.5, 22.5,   0,   0,   0,   0,   35,   0,   0,    0]
     ]
 };
+
+const ITEM_INFO = [
+    ["Green.png",     "Green Shell"],
+    ["Red.png",       "Red Shell"],
+    ["Banana.png",    "Banana"],
+    ["Fib.png",       "Fake Item Box"],
+    ["Mushroom.png",  "Mushroom"],
+    ["Mushroom3.png", "Triple Mushrooms"],
+    ["Bomb.png",      "Bob-omb"],
+    ["Blue.png",      "Blue Shell"],
+    ["Lighting.png",  "Lighting"],
+    ["Star.png",      "Star"],
+    ["Golden.png",    "Golden Mushroom"],
+    ["Mega.png",      "Mega Mushroom"],
+    ["Blooper.png",   "Blooper"],
+    ["Pow.png",       "POW"],
+    ["Tc.png",        "Thunder Cloud"],
+    ["Bill.png",      "Bullet Bill"],
+    ["Green3.png",    "Triple Green Shells"],
+    ["Red3.png",      "Triple Red Shells"],
+    ["Banana3.png",   "Triple Bananas"]
+];
